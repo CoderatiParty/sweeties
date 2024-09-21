665781aba5448578d326dda4bfb576901c3ac064
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-09-21 12:55:27 +0000

    article page and links

--- home/views.py
+++ home/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
-from .models import Article
+from django.shortcuts import render, get_object_or_404
+from .models import Article, Category
 
 # Create your views here.
 
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the home page """
     articles = Article.objects.all().order_by('-date')
+    categories = Category.objects.all().order_by('name')
 
     context = {
         'articles': articles,
+        'categories': categories,
     }
 
     return render(request, 'home/index.html', context)
+
+
+def article(request, article_id):
+    """ A view to show each article in full """
+
+    article = get_object_or_404(Article, pk=article_id)
+
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'home/article.html', context)
