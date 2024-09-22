37cef4fb855d90b95e078e2d73fd79603a1265e2
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-09-22 15:36:15 +0000

    Played with styles. Refactored some css. Added corporate templates .

--- corporate/views.py
+++ corporate/views.py
@@ -1,3 +1,86 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def corporate(request):
+    """ A view to show the main corporate page """
+
+    current_path = request.path
+    referrer = request.META.get('HTTP_REFERER')
+
+    context = {
+        'current_path': current_path,
+        'referrer': referrer,
+    }
+
+    return render(request, 'corporate/corporate_page.html', context)
+
+
+def about(request):
+    """ A view to show the main corporate page """
+
+    current_path = request.path
+    referrer = request.META.get('HTTP_REFERER')
+
+    context = {
+        'current_path': current_path,
+        'referrer': referrer,
+    }
+
+    return render(request, 'corporate/about.html', context)
+
+
+def contact(request):
+    """ A view to show the main corporate page """
+
+    current_path = request.path
+    referrer = request.META.get('HTTP_REFERER')
+
+    context = {
+        'current_path': current_path,
+        'referrer': referrer,
+    }
+
+    return render(request, 'corporate/contact.html', context)
+
+
+def faqs(request):
+    """ A view to show the main corporate page """
+
+    current_path = request.path
+    referrer = request.META.get('HTTP_REFERER')
+
+    context = {
+        'current_path': current_path,
+        'referrer': referrer,
+    }
+
+    return render(request, 'corporate/faqs.html', context)
+
+
+def privacy(request):
+    """ A view to show the main corporate page """
+
+    current_path = request.path
+    referrer = request.META.get('HTTP_REFERER')
+
+    context = {
+        'current_path': current_path,
+        'referrer': referrer,
+    }
+
+    return render(request, 'corporate/privacy.html', context)
+
+
+def terms(request):
+    """ A view to show the main corporate page """
+
+    current_path = request.path
+    referrer = request.META.get('HTTP_REFERER')
+
+    context = {
+        'current_path': current_path,
+        'referrer': referrer,
+    }
+
+    return render(request, 'corporate/terms.html', context)
