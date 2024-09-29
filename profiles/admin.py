776b6a5a9a3c0929db09df9fb02aa1ffabe285bf
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-09-29 15:36:23 +0000

    Styling login pages

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User_Profile
 
-# Register your models here.
+
+class User_ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'member_number',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'email',
+        'street_address1',
+        'street_address2',
+        'town_or_city',
+        'county',
+        'post_or_zipcode',
+        'country',
+    )
+
+
+admin.site.register(User_Profile, User_ProfileAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,4 @@
+import random
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -11,8 +12,15 @@ class User_Profile(models.Model):
     A model for maintaining user's details, subscription information and history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    last_name = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    member_number = models.BigIntegerField(unique=True, editable=False)
     phone_number = models.CharField(max_length=20,
                                             null=True, blank=True)
+    email = models.CharField(max_length=80,
+                                               null=True, blank=True)
     street_address1 = models.CharField(max_length=80,
                                                null=True, blank=True)
     street_address2 = models.CharField(max_length=80,
@@ -28,6 +36,12 @@ class User_Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+    def save(self, *args, **kwargs):
+        if not self.member_number:
+            self.member_number = random.randint(100000000, 999999999)  # Random 9-digit number
+        super().save(*args, **kwargs)
 
 
 @receiver(post_save, sender=User)
@@ -37,5 +51,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         User_Profile.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.user_profile.save()
+    else:
+        # Existing users: Save the profile only if it has been modified
+        instance.user_profile.save()
