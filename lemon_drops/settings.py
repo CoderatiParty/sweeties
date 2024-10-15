7c6c8ec4ad6f128dbbcb3c2aabc6dd96638782bd
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-10-15 14:27:53 +0000

    Initial deployment

--- lemon_drops/settings.py
+++ lemon_drops/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,7 +134,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
