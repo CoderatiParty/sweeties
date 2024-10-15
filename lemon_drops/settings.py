0a9870c83ef9f1302bba8f316a62266921182419
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-10-15 19:31:41 +0000

    Updated settings

--- lemon_drops/settings.py
+++ lemon_drops/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['lemondrops.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lemondrops-7ba75e0d4a2a.herokuapp.com/', 'localhost']
 
 
 # Application definition
