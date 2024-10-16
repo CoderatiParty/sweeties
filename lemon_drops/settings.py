20488030daa4912abf208c2a6a6859acd223df92
Richard Messenger <twmd95gh@students.codeinstitute.net>
2024-10-16 15:18:09 +0000

    Swapping DB

--- lemon_drops/settings.py
+++ lemon_drops/settings.py
@@ -136,21 +136,21 @@ WSGI_APPLICATION = 'lemon_drops.wsgi.application'
 #    }
 #}
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+#if 'DATABASE_URL' in os.environ:
+#    DATABASES = {
+#        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#    }
+#else:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+#    }
 
-#DATABASES = {
-#     'default': dj_database_url.parse('name-here')
-#}
+DATABASES = {
+     'default': dj_database_url.parse('postgres://ufhomuytmtp:5WBGKWNbMDnL@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/finch_salsa_crisp_181769')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
